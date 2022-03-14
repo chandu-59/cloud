557a23bd0413f93da04f7fb77cb6896550abecac
chandu-590 <chandukotaiiitdwd@gmail.com>
2022-03-14 14:20:00 +0530

    added code

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import path from 'path';
 import postRoutes from './routes/posts.js';
 
+var __dirname = path.resolve();
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
@@ -13,7 +14,16 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendfile(
+      path.join(__dirname = 'client/build/index.html')
+    );
+  }
+  )
+}
 const CONNECTION_URL = "mongodb+srv://chandu:Lalithamadhav1!@cluster0.yaanm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
 
